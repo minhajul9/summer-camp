3593eff45f40ec674d7340061bf350c636e4cb44
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-06-14 18:23:42 +0600

    show popular instuctors

--- src/Components/Pages/Home/PopularInstructors.jsx
+++ src/Components/Pages/Home/PopularInstructors.jsx
@@ -3,19 +3,28 @@ import { useState } from "react";
 
 const PopularInstructors = () => {
 
-    const [classes, setClasses] = useState([]);
+    const [instructors, setInstructors] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/instructors/popular')
             .then(res => res.json())
-            .then(data => setClasses(data))
+            .then(data => setInstructors(data))
     }, [])
-    console.log(classes);
+    console.log(instructors);
 
     return (
         <div className="my-24">
             <h1 className='text-center text-4xl font-bold my-8'>Popular Instructors</h1>
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {
+                    instructors.map(instructor => <div key={instructor._id} className="card w-96 bg-base-100 shadow-xl border">
+                        <figure className="bg-black p-2"><img className="h-56 rounded-lg" src={instructor.photo} alt="Image" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{instructor.name}</h2>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
